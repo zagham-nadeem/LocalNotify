a257f250941c5b0acc1e0eacfe412b10be28893a
The Dark Eye <thedarkeye.exe@gmail.com>
2022-05-26 11:49:46 +0500

    files added

---
+++ android/app/src/main/java/com/Notify/MainActivity.java
@@ -0,0 +1,5 @@
+package com.Notify;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import{LocalNotifications,ScheduleOptions}from"@capacitor/local-notifications";
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
-
+  ids:number[]=[];
+ resume:boolean=false;
+  constructor(){}
+  ngOnInit(){
+    setInterval( ()=>{
+      if(this.resume)
+     {
+      this.schedule();
+      console.log('Test', this.ids)
+     }
+    },1000);
 }
+  
+schedule() {
+ var t = new Date();
+  t.setSeconds(t.getSeconds()+5);
+   let id=this.ids.length;
+   this.ids.push(id);
+  let options:ScheduleOptions={notifications:[{
+    id:id,
+    title:"Local Notifications id="+id,
+    body:"Local Notifications body",
+    // schedule:{
+   // at:t,
+   }]
+}
+if(this.resume){
+{
+ LocalNotifications.schedule(options).then(()=>{
+   console.log('test')
+  })
+}
+}
+}}
